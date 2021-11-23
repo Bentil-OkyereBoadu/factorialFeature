a73dc633b3f4e44000f8e630a54e78be995c852c
Bentil-OkyereBoadu <91896057+Bentil-OkyereBoadu@users.noreply.github.com>
2021-11-23 17:27:24 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Calculate = {
+  factorial(number){
+    if(number === 0){
+      return 1;
+    }
+    for(let i = number-1; i >= 1; i--){
+      number *= i;
+    }
+    return number;
+  }
+}
+
+module.exports = Calculate;
+
+
